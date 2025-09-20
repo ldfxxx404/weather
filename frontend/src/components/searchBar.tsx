6bc88975067413c7136f85a9a69cfba2b8beec97
ldfxxx404 <limur9990@gmail.com>
2025-09-20 21:45:55 +0300

    feat: add search bar component

---
+++ frontend/src/components/searchBar.tsx
@@ -0,0 +1,18 @@
+import { type InputHTMLAttributes, type ChangeEvent } from "react";
+
+interface SearchBarProps extends InputHTMLAttributes<HTMLInputElement> {
+  // Props for the search bar component
+  placeholder: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  city: string;
+}
+
+export const SearchBar = (props: SearchBarProps) => {
+  return (
+    <input
+      {...props}
+      type="text"
+      className="p-3 w-full text-white bg-gray-600 rounded-lg focus:outline-none"
+    />
+  );
+};

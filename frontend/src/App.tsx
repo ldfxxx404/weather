88b5afaacf6206e75c3d03de5a305814248443a9
ldfxxx404 <limur9990@gmail.com>
2025-09-21 20:52:56 +0300

    Add new weather icons and utility functions for weather data fetching
    - Added SVG icons for various weather conditions including snowy and thunderstorms.
    - Implemented utility functions `getCoordinates` and `getWeather` for fetching location coordinates and weather data respectively.
    - Introduced TypeScript definitions for SVG imports.
    - Created a mapping for WMO weather codes in Russian.
    - Added a placeholder component for the seven-day weather card.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,88 +1,60 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { SearchBar } from "@/components/searchBar";
+import getCoordinates from "@/utils/getCoordinates";
+import getWeather from "@/utils/getWeather";
+import { WMO_WEATHER_CODES } from "./utils/wmoWeatherCodes";
+
 
 function App() {
-  const [city, setCity] = useState("");
-  const [data, setData] = useState<any>(null);
-  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-  useEffect(() => {
-    if (!city) return;
-
-    fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7&aqi=yes&alerts=no`
-    )
-      .then((res) => res.json())
-      .then((result) => setData(result))
-      .catch((err) => console.error(err));
-  }, [city]);
+  const [city, setCity] = useState(""); // City name
+  const [weekly, setWeekly] = useState<Number[]>([]);
+  const [time, setTime] = useState<String[]>([]);
+  const [code, setCode] = useState([]);
 
   return (
     <main className="min-h-screen bg-[#0B0A17] flex items-center justify-center p-6">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-6xl">
         <div className="flex flex-col gap-6">
-          <input
-            onChange={(e) => setCity(e.target.value)}
-            value={city}
-            type="text"
-            placeholder="Search cities..."
-            className="p-3 w-full text-white bg-gray-600 rounded-lg focus:outline-none"
-          />
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              const coordinates = await getCoordinates(city);
+              if (coordinates) {
+                const data = await getWeather();
+                setWeekly(data.daily.temperature_2m_max);
+                setTime(data.daily.time)
+                setCode(data.daily.weather_code) 
+              }
+            }}
+            >
+            <SearchBar
+              placeholder="Search cities..."
+              city={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </form>
 
           <div className="bg-gray-700 rounded-xl p-6 text-white h-40">
             <h2 className="text-lg font-bold mb-2">Today weather</h2>
-            {data ? (
-              <>
-                <p>
-                  {data.location.name}, {data.location.country}
-                </p>
-                <p>{Math.round(data.current.temp_c)}°C</p>
-                <p>{data.current.condition.text}</p>
-                <p className="absolute ml-96 ">
-                  <img
-                    src={data.current.condition.icon}
-                    alt={data.current.condition.text}
-                  />
-                </p>
-              </>
-            ) : (
-              <p>Loading...</p>
-            )}
           </div>
 
           <div className="bg-gray-700 rounded-xl p-6 text-white h-40">
             <h2 className="text-lg font-bold mb-2">Air condition</h2>
-            {data ? (
-              <>
-                <p>PM2.5: {data.current.air_quality.pm2_5.toFixed(1)}</p>
-                <p>PM10: {data.current.air_quality.pm10.toFixed(1)}</p>
-                <p>CO: {data.current.air_quality.co.toFixed(1)}</p>
-              </>
-            ) : (
-              <p>Loading...</p>
-            )}
           </div>
+            
         </div>
 
         <div className="bg-gray-700 rounded-xl p-6 text-white min-h-[400px]">
           <h2 className="text-lg font-bold mb-4">7 day forecast</h2>
-          {data ? (
-            <ul className="space-y-2">
-              {data.forecast.forecastday.map((day: any, i: number) => (
-                <li key={i} className="flex justify-between">
-                  <span>
-                    {new Date(day.date).toLocaleDateString("ru-RU", {
-                      weekday: "short",
-                      day: "numeric",
-                      month: "short",
-                    })}
-                  </span>
-                  <span>{Math.round(day.day.avgtemp_c)}°C</span>
-                  <span>{day.day.condition.text}</span>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>Loading...</p>
-          )}
+          <ul>
+            {weekly.map((temp: number, idx: number) => (
+              <li key={idx} className="flex justify-between">
+                <span className="text-2xl">{time[idx]}</span>
+                <span className="text-2xl">{temp}°C</span>
+                <span className="text-2xl">{WMO_WEATHER_CODES[code[idx]]}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </main>
---
+++ frontend/src/utils/getWeather.ts
@@ -0,0 +1,25 @@
+import { WEATHER_URL } from "@/constants";
+import { WeatherResponse } from "@/types/responseTypes";
+
+export default async function getWeather(): Promise<WeatherResponse> {
+  try {
+    const { latitude, longitude } = JSON.parse(
+      localStorage.getItem("coordinates") || "{}",
+    );
+    const res = await fetch(
+      `${WEATHER_URL}&latitude=${latitude}&longitude=${longitude}`,
+    );
+    if (!res.ok) {
+      throw new Error("Failed to fetch weather data");
+    }
+    const data = await res.json();
+    console.log("Weather data fetched successfully:", data);
+    if (!data || Object.keys(data).length === 0) {
+      throw new Error("No weather data found");
+    }
+    return data;
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    throw error;
+  }
+}

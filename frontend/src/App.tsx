e43a0af37f2f6ca4859e80df58f9c17ff7068b11
ldfxxx404 <limur9990@gmail.com>
2025-09-24 11:27:26 +0300

    feat: implement TodayForecastCard and SevenDayForecast components with weather data display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import { SearchBar } from "@/components/searchBar";
+import { WMO_WEATHER_CODES } from "@/constants";
+import { SevenDayForecast } from "@/components/sevenDayForecastCard";
+import { reverseDate } from "./utils/reverseDate";
+import { toLocalTime } from "./utils/convertTime";
+import { TodayForecastCard } from "./components/todayForecastCard";
 import getCoordinates from "@/utils/getCoordinates";
 import getWeather from "@/utils/getWeather";
-import { WMO_WEATHER_CODES } from "./utils/wmoWeatherCodes";
-
 
 function App() {
-  const [city, setCity] = useState(""); // City name
-  const [weekly, setWeekly] = useState<Number[]>([]);
-  const [time, setTime] = useState<String[]>([]);
-  const [code, setCode] = useState([]);
+  const [city, setCity] = useState<string>("");
+  const [weekly, setWeekly] = useState<number[]>([]);
+  const [todayWeather, setTodayWeather] = useState<number | null>(null);
+  const [date, setDate] = useState<string[]>([]);
+  const [code, setCode] = useState<number[]>([]);
+  const [sunrise, setSunrise] = useState<string | null>(null);
+  const [sunset, setSunset] = useState<string | null>(null);
 
   return (
     <main className="min-h-screen bg-[#0B0A17] flex items-center justify-center p-6">
@@ -22,40 +28,50 @@ function App() {
               if (coordinates) {
                 const data = await getWeather();
                 setWeekly(data.daily.temperature_2m_max);
-                setTime(data.daily.time)
-                setCode(data.daily.weather_code) 
+                setTodayWeather(data.daily.temperature_2m_max[0]);
+                setDate(data.daily.time);
+                setCode(data.daily.weather_code);
+                setSunrise(data.daily.sunrise[0]);
+                setSunset(data.daily.sunset[0]);
               }
             }}
-            >
+          >
             <SearchBar
               placeholder="Search cities..."
               city={city}
               onChange={(e) => setCity(e.target.value)}
             />
           </form>
+          <TodayForecastCard>
+            <span className="text-lg font-bold">{todayWeather}°C</span>
+            <div className="text-lg font-bold text-right ml-48">
+              <h3>Sunrise 🌇</h3>
+              {sunrise ? <span>{toLocalTime(sunrise)}</span> : "--:--"}
+            </div>
+            <div className="text-lg font-bold text-right pr-9">
+              <h3>Sunset 🌆</h3>
+              {sunset ? <span>{toLocalTime(sunset)}</span> : "--:--"}
+            </div>
+          </TodayForecastCard>
 
-          <div className="bg-gray-700 rounded-xl p-6 text-white h-40">
-            <h2 className="text-lg font-bold mb-2">Today weather</h2>
-          </div>
-
-          <div className="bg-gray-700 rounded-xl p-6 text-white h-40">
+          <div className="bg-gray-700 rounded-xl p-6 text-white h-40 mt-0.5">
             <h2 className="text-lg font-bold mb-2">Air condition</h2>
           </div>
-            
         </div>
-
-        <div className="bg-gray-700 rounded-xl p-6 text-white min-h-[400px]">
-          <h2 className="text-lg font-bold mb-4">7 day forecast</h2>
-          <ul>
-            {weekly.map((temp: number, idx: number) => (
-              <li key={idx} className="flex justify-between">
-                <span className="text-2xl">{time[idx]}</span>
-                <span className="text-2xl">{temp}°C</span>
-                <span className="text-2xl">{WMO_WEATHER_CODES[code[idx]]}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <SevenDayForecast>
+          {weekly.map((temp: number, idx: number) => (
+            <li
+              key={idx}
+              className="flex justify-between items-center border-2 border-gray-600 rounded-lg p-3 bg-gray-800 hover:bg-gray-700 transition-all shadow-sm"
+            >
+              <span className="text-lg font-medium">
+                {reverseDate(date[idx])}
+              </span>
+              <span className="text-lg font-bold">{temp}°C</span>
+              <span className="text-lg">{WMO_WEATHER_CODES[code[idx]]}</span>
+            </li>
+          ))}
+        </SevenDayForecast>
       </div>
     </main>
   );
---
+++ frontend/src/components/todayForecastCard.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+interface TodayForecastCardProps {
+  children: ReactNode;
+}
+
+export const TodayForecastCard = ({ children }: TodayForecastCardProps) => {
+  return (
+    <div className="flex flex-col bg-gray-700 rounded-xl p-6 text-white h-40">
+      <h2 className="text-lg font-bold ">Today weather</h2>
+      <div className="flex justify-between items-center w-full">{children}</div>
+    </div>
+  );
+};

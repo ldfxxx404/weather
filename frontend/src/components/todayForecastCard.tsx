2604f0cf7c64430f9a658fe123b587a66b426051
ldfxxx404 <limur9990@gmail.com>
2025-09-30 14:52:29 +0300

    feat: refactor todayForecastComponent

--- frontend/src/components/todayForecastCard.tsx
+++ frontend/src/components/todayForecastCard.tsx
@@ -1,14 +1,28 @@
 import { ReactNode } from 'react';
+import { Daily } from '@/types/responseTyping';
+import { toLocalTime } from '@/utils/convertTime';
+import { Round } from '@/utils/round';
 
-interface TodayForecastCardProps {
+interface TodayForecastCardProps extends Daily {
   children: ReactNode;
+  todayWeather: number;
 }
 
-export const TodayForecastCard = ({ children }: TodayForecastCardProps) => {
+export const TodayForecastCard = ({ todayWeather, sunrise, sunset }: TodayForecastCardProps) => {
   return (
     <div className="flex flex-col bg-gray-700 rounded-xl p-6 text-white h-40">
       <h2 className="text-lg font-bold ">Today weather 🌡️</h2>
-      <div className="flex justify-between items-center w-full">{children}</div>
+      <div className="flex justify-between items-center w-full">
+        <span className="text-lg font-bold">{Round(todayWeather)}°C</span>
+        <div className="text-lg font-bold text-right ml-48">
+          <h3>Sunrise 🌇</h3>
+          {sunrise ? <span>{toLocalTime(sunrise)}</span> : '--:--'}
+        </div>
+        <div className="text-lg font-bold text-right pr-9">
+          <h3>Sunset 🌆</h3>
+          {sunset ? <span>{toLocalTime(sunset)}</span> : '--:--'}
+        </div>
+      </div>
     </div>
   );
 };

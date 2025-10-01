d5f9a6022c0f8d5da0cb4cfea6734ca15b194373
ldfxxx404 <limur9990@gmail.com>
2025-10-01 17:10:20 +0300

    feat(hourlyForecastCard): fix mobile layout

--- frontend/src/components/hourlyForecastCard.tsx
+++ frontend/src/components/hourlyForecastCard.tsx
@@ -1,14 +1,26 @@
+import { Round } from '@/utils/round';
 import { ReactNode } from 'react';
 
 interface hourlyForecastCardProps {
   children: ReactNode;
+  hourlyTemp: number;
+  hourlyRainChance: number;
 }
 
-export const HourlyForecastCard = ({ children }: hourlyForecastCardProps) => {
+export const HourlyForecastCard = ({ hourlyRainChance, hourlyTemp }: hourlyForecastCardProps) => {
   return (
-    <div className="bg-gray-700 rounded-xl p-6 text-white h-40 w-full ml-[50%]">
+
+    <div className="bg-gray-700 rounded-xl p-6 text-white max-h-40 w-full ml-[50%] overflow-y-hidden no-scrollbar max-sm:ml-0">
       <h2 className="font-bold text-lg">Hourly forecast</h2>
-      {children}
+      <div>
+        <h3 className="text-lg font-bold">
+          Current temperature: <span className="font-medium text-base">{Round(hourlyTemp)} °C</span>
+        </h3>
+        <h3 className="text-lg font-bold">
+          Chance of rain: <span className="font-medium text-base">{hourlyRainChance}</span>
+        </h3>
+      </div>
     </div>
+
   );
 };
